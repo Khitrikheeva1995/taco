967658eb399cbd2fa06ff40a168ecb3be6c7ca18
Polina Khitrikheeva <polina.khitrikheeva@yandex.ru>
2015-10-30 12:53:16 +0800

    Login and MainWindow added.

---
+++ src/MainWindow.java
@@ -0,0 +1,15 @@
+import javax.accessibility.AccessibleContext;
+import javax.swing.*;
+
+/**
+ * Created by User14 on 30.10.15.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super();
+
+        this.setVisible(true);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
